aa759e4482ebcee95c4adfc6a826dcabae34d709
NeilNeel <neelbarvaliya04@gmail.com>
2025-03-22 14:05:41 -0400

    implement toggle functionality for function containers in JavaScript and update styles for visibility

--- script.js
+++ script.js
@@ -6,3 +6,41 @@ const DomElementBtnCheckPalindrome = document.querySelector(
   ".btn-check-palindrome"
 );
 const DomElementBtnTotalBill = document.querySelector(".btn-total-bill");
+const DomElementFunctionContainerReverse = document.querySelector(
+  ".function-container--reverse"
+);
+const DomElementFunctionContainerPalindrome = document.querySelector(
+  ".function-container--palindrome"
+);
+const DomElementFunctionContainerTotalBill = document.querySelector(
+  ".function-container--total-bill"
+);
+
+// FUNCTION TOGGLE
+const toggleFunctionContainer = (container) => {
+  const containers = [
+    DomElementFunctionContainerReverse,
+    DomElementFunctionContainerPalindrome,
+    DomElementFunctionContainerTotalBill,
+  ];
+
+  containers.forEach((cont) => {
+    if (cont !== container) {
+      cont.classList.remove("function-container--active");
+    }
+  });
+
+  container.classList.add("function-container--active");
+};
+
+DomElementBtnStringReverse.addEventListener("click", () => {
+  toggleFunctionContainer(DomElementFunctionContainerReverse);
+});
+
+DomElementBtnCheckPalindrome.addEventListener("click", () => {
+  toggleFunctionContainer(DomElementFunctionContainerPalindrome);
+});
+
+DomElementBtnTotalBill.addEventListener("click", () => {
+  toggleFunctionContainer(DomElementFunctionContainerTotalBill);
+});
