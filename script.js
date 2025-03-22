53b1c4e9b9a5d5957ce8dcab0b165fab84987831
NeilNeel <neelbarvaliya04@gmail.com>
2025-03-22 13:14:26 -0400

    refactor: enhance HTML structure and styles; add JavaScript functionality for string reversal, palindrome check, and total bill calculation

---
+++ script.js
@@ -0,0 +1,8 @@
+// DOM ELEMENTS
+const DomElementBtnStringReverse = document.querySelector(
+  ".btn-reverse-string"
+);
+const DomElementBtnCheckPalindrome = document.querySelector(
+  ".btn-check-palindrome"
+);
+const DomElementBtnTotalBill = document.querySelector(".btn-total-bill");
